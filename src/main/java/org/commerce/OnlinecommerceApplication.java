78fff695848ca3b9e20d26fc190b1f0bf2b506ae
Jayadeep Gilroy <jayadeep.gilroy@altisource.com>
2017-04-29 12:50:43 -0400

    Initial Commit

---
+++ src/main/java/org/commerce/OnlinecommerceApplication.java
@@ -0,0 +1,12 @@
+package org.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinecommerceApplication.class, args);
+	}
+}
---
+++ src/test/java/org/commerce/OnlinecommerceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.commerce;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlinecommerceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
